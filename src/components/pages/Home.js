c87a31462f97d79fa29585f85921ea093abad64d
Tammy Gegoski <tgegoski@gmail.com>
2022-03-10 21:55:22 -0500

    changed titles

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Home() {
     return (
-        <h1>Home Page!</h1>        
+        <h1>Welcome!</h1>        
     )
 }
 export default Home;
--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Projects() {
     return (
-        <h1>Projects Page!</h1>        
+        <h1>My Projects</h1>        
     )
 }
 export default Projects;
